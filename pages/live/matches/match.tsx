fd085f48caac264f82da7c463486a0f891a5ed43
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-04-03 12:44:30 +0100

    Update match.tsx

--- pages/live/matches/match.tsx
+++ pages/live/matches/match.tsx
@@ -1,3 +1,4 @@
+import Heading from "@/components/live/Heading";
 import LiveLayout from "@/components/live/LiveLayout";
 import { ThemeContext } from "@/pages/_app";
 import { matchData } from "@/types";
@@ -9,20 +10,23 @@ const Match = () => {
   const router = useRouter();
   const data = router.query;
   console.log(data);
-  const mode = useContext(ThemeContext);
-
+  const { mode } = useContext(ThemeContext);
+  const homeForm = data?.homeTeamForm?.split("");
+  const awayForm = data?.awayTeamForm?.split("");
   if (data) {
     return (
       <LiveLayout>
         <main
           className={`w-full min-h-screen sm:p-12 px-6 py-12  ${
             mode == "dark" && "bg-[#111]"
-          }`}
+          } items-center flex flex-col`}
         >
-          <div className="w-full rounded-md h-60 bg-pry-color flex justify-center">
+          <div className="w-full rounded-md h-60 bg-pry-color flex justify-center mb-6">
             <div className="md:w-[60%] flex justify-around items-center w-full relative">
               <div
-                className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-xl font-semibold`}
+                className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ${
+                  mode == "dark" ? "text-[#252525]" : "text-white"
+                } text-xl font-semibold`}
               >
                 {data.status}
               </div>
@@ -35,7 +39,9 @@ const Match = () => {
                     height={60}
                     priority={true}
                   />
-                  <p className={`text-sm font-semibold text-white `}>
+                  <p
+                    className={`text-sm font-semibold text-white text-center `}
+                  >
                     {data.homeTeamName}
                   </p>
                 </div>
@@ -49,13 +55,91 @@ const Match = () => {
                     height={60}
                     priority={true}
                   />{" "}
-                  <p className={`text-sm font-semibold text-white `}>
+                  <p className={`text-sm font-semibold text-white text-center`}>
                     {data.awayTeamName}
                   </p>
                 </div>
               </section>
+              <div
+                className={`absolute bottom-4 left-[50%] translate-x-[-50%] ${
+                  mode == "dark" ? "text-[#252525]" : "text-white"
+                } text-base font-semibold`}
+              >
+                {data.venue}
+              </div>
             </div>
           </div>
+          <Heading heading="Form (Last 5 matches)" />
+          <section className="flex md:w-[60%] w-full pb-6 justify-around">
+            <div>
+              <p className={`font-bold text-xl`}>
+                {homeForm?.map((form: string) => (
+                  <span
+                    className={`ml-[1px] ${
+                      form == "W"
+                        ? "text-green-600"
+                        : form == "L"
+                        ? "text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    {form}
+                  </span>
+                ))}
+              </p>
+            </div>
+            <div>
+              {" "}
+              <p className={`font-bold text-xl`}>
+                {awayForm?.map((form: string) => (
+                  <span
+                    className={`ml-[1px] ${
+                      form == "W"
+                        ? "text-green-600"
+                        : form == "L"
+                        ? "text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    {form}
+                  </span>
+                ))}
+              </p>
+            </div>
+          </section>
+          <Heading heading="Odds" />
+          <section className="flex md:w-[60%] w-full gap-2 pb-6 justify-around">
+            <div
+              className={`rounded-md font-bold w-40 flex justify-between p-2 ${
+                mode == "dark"
+                  ? "bg-gray-700 text-gray-300"
+                  : "text-gray-700 bg-gray-300"
+              } `}
+            >
+              <span>1</span>
+              {data?.homeTeamOdds}
+            </div>{" "}
+            <div
+              className={`rounded-md font-bold w-40 flex justify-between p-2 ${
+                mode == "dark"
+                  ? "bg-gray-700 text-gray-300"
+                  : "text-gray-700 bg-gray-300"
+              } `}
+            >
+              <span>X</span>
+              {data?.drawOdds}
+            </div>{" "}
+            <div
+              className={`rounded-md font-bold w-40 flex justify-between p-2 ${
+                mode == "dark"
+                  ? "bg-gray-700 text-gray-300"
+                  : "text-gray-700 bg-gray-300"
+              } `}
+            >
+              <span>2</span>
+              {data?.awayTeamOdds}
+            </div>{" "}
+          </section>
         </main>
       </LiveLayout>
     );
